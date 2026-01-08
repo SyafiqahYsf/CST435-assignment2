986e7930bc92bc113af050cbf6ad9958dcf3f738
SyafiqahYsf <117891845+SyafiqahYsf@users.noreply.github.com>
2026-01-08 23:37:08 +0800

    Update concurrent_image_processing.py

--- concurrent_image_processing.py
+++ concurrent_image_processing.py
@@ -4,42 +4,13 @@ import os
 import time
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
+# Import the tasks from the tasks folder
+from tasks import task1_grayscale, task2_blur, task3_edges, task4_sharpen, task5_brightness
+
 # ================= Configuration Parameters =================
 INPUT_DIR = "food-101-subset" # Data source folder containing 250 Food-101 i>
 OUTPUT_DIR = "output_concurrent" # Output path
 WORKER_COUNTS = [1, 2, 4, 8]  # Number of processes to test
-
-# ---------------- Image Processing Function ----------------
-def apply_image_filters(data):
-    image_path, output_folder = data
-    img = cv2.imread(image_path)
-    if img is None:
-        return None
-    file_name = os.path.basename(image_path)
-
-    # Filter 1: Grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # Filter 2: Gaussian Blur
-    blur = cv2.GaussianBlur(gray, (3, 3), 0)
-    
-    # Filter 3: Sobel Edge Detection
-    sobel_x = cv2.Sobel(blur, cv2.CV_64F, 1, 0, ksize=3)
-    sobel_y = cv2.Sobel(blur, cv2.CV_64F, 0, 1, ksize=3)
-    edges = cv2.convertScaleAbs(cv2.magnitude(sobel_x, sobel_y))
-    
-    # Filter 4: Sharpen
-    sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    sharpened = cv2.filter2D(edges, -1, sharpen_kernel)
-    
-    # Filter 5: Brightness
-    final_image = cv2.convertScaleAbs(sharpened, alpha=1.0, beta=30)
-    
-    # Save processed image
-    output_path = os.path.join(output_folder, f"concurrent_{file_name}")
-    cv2.imwrite(output_path, final_image)
-    
-    return file_name
 
 # ---------------- Run Processing using concurrent.futures ----------------
 def run_concurrent_test(image_paths, workers):
